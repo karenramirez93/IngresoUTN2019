615f9804f7110b72c175a519e9837b393bb7cf69
karenramirez93 <47385927+karenramirez93@users.noreply.github.com>
2019-02-11 23:29:34 -0300

    Add files via upload

---
+++ jsEntradaSalida-9.js
@@ -0,0 +1,30 @@
+/*Debemos lograr tomar el importe por ID ,
+transformarlo a entero (parseInt), luego
+mostrar el importe con un aumento del 10 %
+en el cuadro de texto "RESULTADO".*/
+function MostrarAumento()
+{
+var sueldo;
+var aumento;
+var sueldoconaumento;
+ sueldo=document.getElementById("sueldo").Value;
+ sueldo=parseInt(sueldo);
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+}
