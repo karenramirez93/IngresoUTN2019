ed1c8f74d33882b09b18cfd72ccd42044faf3b28
karenramirez93 <47385927+karenramirez93@users.noreply.github.com>
2019-02-14 11:54:44 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,10 +1,21 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+edad=parseInt(document.getElementById("edad").value);
+if (edad<13)
+{
+    alert("Es un niño");
 
+}
+else if (edad<17)
+{
+    alert("Sos Adolescente");
 
+}
 
-
-
+else{
+    alert("sos adulto");
+}
 
 }//FIN DE LA FUNCIÓN
