b940980c3feffc678dd95a7eecf049a0dacd9e15
karenramirez93 <47385927+karenramirez93@users.noreply.github.com>
2019-02-14 23:12:03 -0300

    Add files via upload

---
+++ jsInstruccionIF (3).js
@@ -0,0 +1,27 @@
+function Mostrar()
+{
+//tomo la edad  
+var edad;
+edad=parseInt(document.getElementById("edad").value);
+
+
+
+
+
+if (edad>17)
+{
+    alert("Eres mayor de edad");
+
+
+
+
+}
+
+
+
+
+    
+
+
+
+}//FIN DE LA FUNCIÓN
