b484c99ee6e7f1f1603a737b9d22102d141e483d
karenramirez93 <47385927+karenramirez93@users.noreply.github.com>
2019-02-27 12:36:14 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,11 +4,37 @@ function Mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
-	var respuesta='si';
+	var numero;
+	var respuesta=true;
 
+
+while (respuesta==true)
+{
+	numero=parseInt(prompt("Ingrese un numero"));
+	 
+	while (isNaN(numero))
+	{
+		alert("No es un numero");
+		numero=parseInt(prompt("Ingrese un numero"));
+	}
+	if (numero>=0)
+	{
+		positivo=positivo+numero;
+
+	}
+    else {
+		negativo=negativo*numero;
+		contador=contador+1;
+
+	}
+	respuesta=confirm("desea continuar?");
+}
 
 document.getElementById('suma').value=positivo;
+if (contador==0)
+{
+	negativo=0;
+}
 document.getElementById('producto').value=negativo;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,16 +1,39 @@
 function Mostrar()
 {
 
-	var contador=0;
+	var flag=0;
 	// declarar variables
-	
-	var respuesta='si';
+	var maximo;
+	var minimo;
+	var numero;
 
-	while(respuesta!='no')
-	{
-		
 	
+	var respuesta=true;
+
+	while(respuesta!=false);
+	{
+		numero=parseInt(prompt("ingrese un numero"));
+		while (isNaN(numero))
+		{
+			numero=parseInt(prompt("no es un numero ,ingrese un numero"));
+		}
+	if (flag==0||numero>maximo)
+	{
+		maximo=numero;
+
 	}
+
+	if (flag==0||numero<minimo)
+	{
+		minimo=numero;
+		flag=1;
+
+	}
+	respuesta=confirm("desea continuar?");
+
+	}
+	document.getElementById("maximo").value=maximo;
+	document.getElementById("minimo").value=minimo;
 
 
 
